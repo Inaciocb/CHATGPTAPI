9ab5ed380fe078e93602fa64456357a31f39bc0f
Inácio <67322986+Inaciocb@users.noreply.github.com>
2023-04-03 11:08:47 -0300

    Update script1.py

--- script1.py
+++ script1.py
@@ -1,6 +1,6 @@
 import openai
 
-openai.api_key = "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"
+openai.api_key = "inserir a chave entre as aspas" 
 
 completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": "Me dê 3 ideias de aplicativos que eu posso criar com as api´s da openai"}])
 print(completion.choices[0].message.content)
